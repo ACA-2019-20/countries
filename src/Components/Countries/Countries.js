b00bbf1d979d920f123f6976de36fc17fa4b827d
Yervand Stepanyan <yervand.stepanyan@gmail.com>
2020-01-12 17:02:38 +0400

    Initial commit

--- src/Components/Countries/Countries.js
+++ src/Components/Countries/Countries.js
@@ -16,28 +16,36 @@ export default class Countries extends React.Component {
   componentDidMount() {
     this.setState({isLoading: true});
 
-    fetch('https://restcountries.eu/rest/v2/all')
-      .then(res => res.json())
-      .then(res => this.setState({countries: res, isEmpty: res.length === 0, isLoading: false}))
-      .catch(err => this.setState({isLoading: false, errorText: err.message}));
+    this.fetchCountries();
   }
+
+  fetchCountries = (countryName = "") => {
+    if (countryName) {
+      fetch(`https://restcountries.eu/rest/v2/name/${countryName}`)
+        .then(res => res.json())
+        .then((res) => {
+          if (res.status === 404) {
+            throw new Error('No Data')
+          }
+          return res;
+        })
+        .then(res => this.setState({
+          countries: res, isEmpty: res.length === 0,
+          isLoading: false, errorText: ""
+        }))
+        .catch(err => this.setState({isLoading: false, errorText: err.message, countries: []}));
+    } else {
+      fetch('https://restcountries.eu/rest/v2/all')
+        .then(res => res.json())
+        .then(res => this.setState({countries: res, isEmpty: res.length === 0, isLoading: false}))
+        .catch(err => this.setState({isLoading: false, errorText: err.message}));
+    }
+  };
 
   searchCountry = (countryName) => {
     this.setState({isLoading: true});
 
-    fetch(`https://restcountries.eu/rest/v2/name/${countryName}`)
-      .then(res => res.json())
-      .then((res) => {
-        if (res.status === 404) {
-          throw new Error('No Data')
-        }
-        return res;
-      })
-      .then(res => this.setState({
-        countries: res, isEmpty: res.length === 0,
-        isLoading: false, errorText: ""
-      }))
-      .catch(err => this.setState({isLoading: false, errorText: err.message, countries: []}));
+    this.fetchCountries(countryName);
   };
 
   render() {
